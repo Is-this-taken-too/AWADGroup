0848151e75dbe36587bf38b48e7fcc7051c9ab21
2013 <u01man12@ews83x.uoa.abdn.ac.uk>
2013-11-12 17:13:00 +0000

    Should track more

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Group::Application.config.secret_token = 'ac31c02af092874251c1408f3ec81d800a0d68f9e2a4409ce94fd12f0af60a90800d6c0b0d3334efd4b1e1e2d27f8c0f9bb4293019642e5a328c99b842f12e13'
