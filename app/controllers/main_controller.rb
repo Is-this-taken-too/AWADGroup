b1a2c7d746b10b8e7c7628a6fcb9bdcc5259b725
Simon de Lecq <sidelecq@gmail.com>
2013-11-13 00:09:49 +0000

    Graphs, good stuff, routing etc.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,12 +1,3 @@
 class MainController < ApplicationController
-def donors
-	donors = Donors.all
-	@donorsRegularTotal = donors.sum(:REGULAR)
-	@donorsOtherTotal = donors.sum(:OTHER)
-	@donorsTotal = donors.sum(:TOTAL)
-	
-	
-	
-end
 
 end
