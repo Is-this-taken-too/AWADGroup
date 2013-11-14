a90971a648a6d71cfdf58087f74f92e46f59860a
Michael Nicholson <m.nicholson.12@aberdeen.ac.uk>
2013-11-14 20:43:58 +0000

    Refinement, search function

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,10 +1,19 @@
 class ProjectController < ApplicationController
 	def index
 		@project = Project.all
+			if session[:user_id] != nil
+     
+				@user = User.find(session[:user_id]).email
+     
+			else
+      
+				@user = "Guest"
+			end
+		
 	end
 	
 	def search
-		
+		@project= Project.find(:all, :conditions => ["COUNTRY_OPERATING_UNIT LIKE ?", "%#{params[:key]}%"])
 	end
 	
 
